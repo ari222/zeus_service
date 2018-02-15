70c98a48be6421b48f260e6a0b49cf942ab8afc3
sudipto <sudiptomukh@gmail.com>
2018-02-15 16:02:06 +0530

    first checkin

--- server.js
+++ server.js
@@ -16,7 +16,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(routes);
 
 //   ********* SHOULD BE UNCOMMENTED TO RUN LOCALLY  *********
-// app.listen(8080, () => {
-//   console.log('listening ...');
-// });
+ app.listen(8080, () => {
+   console.log('listening ...');
+ });
 module.exports = app;
